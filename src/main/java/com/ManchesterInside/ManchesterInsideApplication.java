42e71228235227189fdf74ccf5dc123aa272b895
wyaaung <wya.aung@gmail.com>
2022-08-09 20:23:04 +0100

    Changed to Maven

--- src/main/java/com/ManchesterInside/ManchesterInsideApplication.java
+++ src/main/java/com/ManchesterInside/ManchesterInsideApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ManchesterInside {
+public class ManchesterInsideApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ManchesterInside.class, args);
+		SpringApplication.run(ManchesterInsideApplication.class, args);
 	}
 
 }
