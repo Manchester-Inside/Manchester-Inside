9f6bcc18e7e5ae108a7509067a0fbc469fcbab3b
Wai Aung <2392waau@nibe.se>
2022-08-09 14:45:10 +0100

    First Changes:
    - Put some configs
    - Added dependencies: Security, DB

---
+++ src/main/java/manchesterinside/ManchesterInside.java
@@ -0,0 +1,13 @@
+package manchesterinside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManchesterInsideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManchesterInsideApplication.class, args);
+	}
+
+}
