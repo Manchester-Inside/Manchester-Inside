fbef9f499d1d1ff29e3e1a9f210de835e3a49a87
wyaaung <wya.aung@gmail.com>
2022-08-09 20:07:11 +0100

    Name Changes

--- src/main/java/manchesterinside/config/Security.java
+++ src/main/java/manchesterinside/config/Security.java
@@ -1,4 +1,4 @@
-package hello.config;
+package manchesterinside.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/manchesterinside/config/StandaloneViews.java
+++ src/main/java/manchesterinside/config/StandaloneViews.java
@@ -1,4 +1,4 @@
-package hello.config;
+package manchesterinside.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
