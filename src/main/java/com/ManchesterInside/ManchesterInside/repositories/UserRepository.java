ae5b8939164e02ac67fd32be333d78bf23da4acf
Wai Aung <2392waau@nibe.se>
2022-08-11 12:48:22 +0100

    Init Repo Folder

---
+++ src/main/java/com/ManchesterInside/ManchesterInside/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.ManchesterInside.ManchesterInside.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ManchesterInside.ManchesterInside.entities.User;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+    
+}
