891c5594249d169b4d240316f9b9b27b9c888849
r13355ty <taeseung.you@student.manchester.ac.uk>
2022-09-05 08:23:39 +0100

    view courses

--- src/main/java/com/ManchesterInside/ManchesterInside/entities/Course.java
+++ src/main/java/com/ManchesterInside/ManchesterInside/entities/Course.java
@@ -1,13 +1,16 @@
 package com.ManchesterInside.ManchesterInside.entities;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -34,6 +37,7 @@ public class Course {
 	@NotNull
 	@ManyToOne
 	private School school;
+	
 
 	public long getId() {
 		return id;
